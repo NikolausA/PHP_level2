fd7769a073a1dd6e61c180a7fee4407f59bf6547
NikolausA <nik.aleksenko@gmail.com>
2020-04-24 16:02:27 +0300

    ДЗ к третьему уроку

---
+++ Lesson3/index.php
@@ -0,0 +1,25 @@
+<?php
+const PHOTO_PATH = 'small/';
+const PHOTO_BIG_PATH = 'big/';
+
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+try {
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('index.tmpl');
+    
+$arr_photo = array_slice(scandir(PHOTO_BIG_PATH), 2);
+    
+echo $template->render([
+    'title' => 'Фото альбом',
+    'small_photo_path' => 'PHOTO_PATH',
+    'photos' => $arr_photo
+]);
+}
+catch (Exseption $e) {
+die('ERROR ' . $e->getMessage());
+}
+?>
---
+++ Lesson3/photo.php
@@ -0,0 +1,27 @@
+<?php
+const PHOTO_PATH = 'small/';
+const PHOTO_BIG_PATH = 'big/';
+
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+try {
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
+$template = $twig->loadTemplate('photo.tmpl');
+    
+$photo = $_GET['photo'];
+if (!file_exists(PHOTO_BIG_PATH . $photo)) throw new Exception ('There is not photo');
+    
+echo $template->render([
+    'title' => 'Фотография ',
+    'big_photo_path' => 'PHOTO_BIG_PATH',
+    'photo' => $photo
+]);
+}
+catch (Exseption $e) {
+die('ERROR ' . $e->getMessage());
+}
+    
+?>
